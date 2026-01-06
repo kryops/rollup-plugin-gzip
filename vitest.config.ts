aea72647a662e99689570ea68a2ede4c2e54d004
Michael Manzinger <michael@kryops.de>
2026-01-06 10:53:04 +0100

    Update dev dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     globals: true,
-    poolOptions: {
-      forks: {
-        minForks: 1,
-        maxForks: 1,
-      },
-    },
+    maxWorkers: 1,
   },
 })
